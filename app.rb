ffc7de32759758a64a08dab7a25b8cc3368120b2
stephenyoon84 <stephenyoon84@gmail.com>
2019-03-12 10:25:45 -0400

    add solution

--- app.rb
+++ app.rb
@@ -4,6 +4,13 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
-
+    get('/'){erb :root}
+    get('/new'){erb :"pirates/new"}
+    post '/pirates' do
+      @pirate1 = Pirate.new(params[:pirate])
+      @ship1 = Ship.new(params[:pirate][:ships][0])
+      @ship2 = Ship.new(params[:pirate][:ships][1])
+      erb :"pirates/show"
+    end
   end
 end
